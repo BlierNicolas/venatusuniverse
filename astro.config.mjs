67ea100bffc8733036c99a3f7a3897030fb0f606
Nicolas <nicomeccool@gmail.com>
2026-03-13 22:15:48 -0400

    Work on homepage

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,5 +3,13 @@ import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
-    site: "https://venatusuniverse.com"
+    site: "https://venatusuniverse.com",
+    i18n: 
+    {
+        locales: ["fr", "en"],
+        defaultLocale: "fr",
+        routing: {
+            prefixDefaultLocale: false
+        }
+    }
 });
